fe0dad085b51bae83b0f511f393b7ece2cd23380
codingpizza420 <dtlatelpa123@gmail.com>
2025-11-12 04:01:13 -0500

    bin file makes sure dependencies are installed, and file being able to run anywhere on the system

--- bin.js
+++ bin.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { spawnSync, spawn } from "child_process";
+import { spawn } from "child_process";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 import fs from "fs";
@@ -13,19 +13,24 @@ const projectRoot = resolve(__dirname);
 // The direct script
 const entry = resolve(projectRoot, "Tui.tsx");
 
-// Intsalling all the dependecies
+// !!!correct dependencies installed !!!
 if (!fs.existsSync(resolve(projectRoot, "node_modules"))) 
 {
-  console.log("Currently Installing Dependencies");
-  spawnSync("npm", ["install"], { cwd: projectRoot, stdio: "inherit" });
+  console.log("Installing dependencies...");
+  const installCmd = process.platform === "win32" ? "npm.cmd" : "npm";
+  spawn(installCmd, ["install"], { cwd: projectRoot, stdio: "inherit" });
 }
 
 
-const child = spawn("npx", ["tsx", entry], 
-{
-  cwd: projectRoot,  // Always changing the current working directory
-  stdio: "inherit"
+// firguring out windows and macos command
+const npxCmd = process.platform === "win32" ? "npx.cmd" : "npx";
+
+
+const child = spawn(npxCmd, ["tsx", entry], {
+  cwd: projectRoot,
+  stdio: "inherit",
+  shell: false,
 });
 
-
 child.on("exit", (code) => process.exit(code));
+
